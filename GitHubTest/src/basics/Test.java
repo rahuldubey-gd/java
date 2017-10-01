4eb73f72576f4fe7d6fdb2a71b75be002964ca8e
Rahul Dubey <Rahul Dubey@192.168.2.5>
2017-10-01 11:58:16 +0530

    //Variable chnages

--- GitHubTest/src/basics/Test.java
+++ GitHubTest/src/basics/Test.java
@@ -5,7 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hellooo......Bro Its GITHub");
-
+		//Eclipse GIT Test
+		int a=20;
 	}
 
 }
