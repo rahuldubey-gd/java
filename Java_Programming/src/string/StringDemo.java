d1f07a6f1494fff5484bd642650b16877cbcfd72
rahuldubey-gd <rahuldubey.gd@gmail.com>
2020-11-03 16:58:57 +0530

    string demo

---
+++ Java_Programming/src/string/StringDemo.java
@@ -0,0 +1,21 @@
+package string;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		
+		String name = "Rahul";
+		
+		System.out.println(name);
+		
+		char ch[] = {'R','a','h','u','l'};
+		
+		System.out.println(ch);
+		
+		String name1 = new String("Rahul");
+		
+		System.out.println(name1);
+
+	}
+
+}
